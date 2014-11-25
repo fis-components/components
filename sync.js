7cc887d5f830ffc192e9a95240f25e48aa9686c1
xiangshouding <fansekey@gmail.com>
2014-11-25 22:36:43 +0800

    test

--- sync.js
+++ sync.js
@@ -1,1 +1,28 @@
-console.log(process.argv);
+var TOKEN = process.argv[2];
+
+if (!TOKEN) {
+    process.stderr.write('ERROR, must given token of GITHUB!\n');
+    return;
+}
+
+var GithubAPI = require('github');
+
+var github = new GithubAPI({
+    version: '3.0.0'
+});
+
+github.authenticate({
+    type: "token",
+    token: TOKEN
+});
+
+github.repos.createFromOrg({
+    name: 'test',
+    org: 'fis-components'
+}, function (err, data) {
+    if (err) {
+        throw err;
+        return;
+    }
+    process.exit(0);
+});
