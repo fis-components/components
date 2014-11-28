1f4a58daf5d0b608810dfc622aedd0f12bfdcbc3
xiangshouding <fansekey@gmail.com>
2014-11-28 08:44:35 +0800

    add underscore.js

---
+++ modules/underscore.js
@@ -0,0 +1,19 @@
+'use strict';
+
+module.exports = (function() {
+    return [{
+        repos: 'https://github.com/jashkenas/underscore.git',
+        version: '1.7.0',
+        main: 'underscore.js',
+        mapping: [
+            {
+                reg: "underscore.js",
+                release: '$&'
+            },
+            {
+                reg: '*',
+                release: false
+            }
+        ]    
+    }]
+})();
