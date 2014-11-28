84ab7e367dd97b96561e4be78141b8956f33b204
xiangshouding <fansekey@gmail.com>
2014-11-28 13:19:17 +0800

    jquery-ui

--- modules/jquery-ui.js
+++ modules/jquery-ui.js
@@ -5,6 +5,7 @@ module.exports = (function() {
         repos: 'https://github.com/jquery/jquery-ui.git',
         version: '1.11.2',
         main: 'jquery-ui.js',
+        description: 'jquery-ui',
         build: 'npm install && npm install grunt-cli && ./node_modules/.bin/grunt concat',
         dependencies: [
             "github:fis-components/jquery@>=1.6"
