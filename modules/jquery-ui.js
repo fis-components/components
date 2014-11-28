b7acf1c4685b2e3de6023f98ee0be11da2e74450
廖学芝 <2betop.cn@gmail.com>
2014-11-28 18:00:29 +0800

    Update jquery-ui.js

--- modules/jquery-ui.js
+++ modules/jquery-ui.js
@@ -20,7 +20,7 @@ module.exports = (function() {
                 release: '$&'
             },
             {
-                reg: /\/dist\/(.*?)$/,
+                reg: /^\/dist\/(.*?)$/,
                 release: '$1'
             },
             {
