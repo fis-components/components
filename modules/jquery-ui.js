9fe9571b5d545d1208eadb680cb634a64f217a7a
廖学芝 <2betop.cn@gmail.com>
2014-12-01 10:48:54 +0800

    Update jquery-ui.js

--- modules/jquery-ui.js
+++ modules/jquery-ui.js
@@ -4,7 +4,7 @@ module.exports = (function() {
     return [{
         repos: 'https://github.com/jquery/jquery-ui.git',
         version: '1.11.2',
-        main: 'jquery-ui.js',
+        main: 'all.js',
         description: 'jquery-ui',
         build: 'npm install && npm install grunt-cli && ./node_modules/.bin/grunt concat',
         dependencies: [
@@ -12,12 +12,20 @@ module.exports = (function() {
         ],
         mapping: [
             {
-                reg: '/ui/**',
-                release: '$&'
+                reg: /^\/ui\/(.*?)$/,
+                release: '$1'
             },
             {
                 reg: '/themes/**',
                 release: '$&'
+            },
+            {
+                reg: '/dist/jquery-ui.js',
+                release: 'all.js'
+            },
+            {
+                reg: '/dist/jquery-ui.css',
+                release: 'all.css'
             },
             {
                 reg: /^\/dist\/(.*?)$/,
