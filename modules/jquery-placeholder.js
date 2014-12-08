b0b499b661fec1f6e4b6a9aad8d41bafac3fc962
廖学芝 <2betop.cn@gmail.com>
2014-12-08 23:44:41 +0800

    Update jquery-placeholder.js

--- modules/jquery-placeholder.js
+++ modules/jquery-placeholder.js
@@ -6,7 +6,7 @@ module.exports = (function() {
         github: {
           author: "fis-components"
         },
-        repos: 'https://github.com/mathiasbynens/jquery-placeholder',
+        repos: 'https://github.com/mathiasbynens/jquery-placeholder.git',
         version: 'v2.0.8',
         main: 'jquery.placeholder.js',
         description: 'A jQuery plugin that enables HTML5 placeholder behavior for browsers that aren’t trying hard enough yet https://mths.be/placeholder',
