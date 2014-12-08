9403522478baef171904bf6faf8bc34debb6e59e
廖学芝 <2betop.cn@gmail.com>
2014-12-08 23:23:16 +0800

    Create jquery-placeholder.js

---
+++ modules/jquery-placeholder.js
@@ -0,0 +1,28 @@
+'use strict';
+
+module.exports = (function() {
+    return [{
+        protocol: "github",
+        github: {
+          author: "fis-components"
+        },
+        repos: 'https://github.com/mathiasbynens/jquery-placeholder',
+        version: '2.0.8',
+        main: 'jquery.placeholder.js',
+        description: 'A jQuery plugin that enables HTML5 placeholder behavior for browsers that aren’t trying hard enough yet
+https://mths.be/placeholder',
+        dependencies: [
+            "jquery@>=1.6"
+        ],
+        mapping: [
+            {
+                reg: 'query.placeholder.js',
+                release: '$&'
+            },
+            {
+                reg: '*',
+                release: false
+            }
+        ]
+    }];
+})();
