35e53196c4b65df38698a66ab8bfadf8d42bc80c
廖学芝 <2betop.cn@gmail.com>
2014-12-09 15:17:42 +0800

    Update font-awesome.js

--- modules/font-awesome.js
+++ modules/font-awesome.js
@@ -8,7 +8,7 @@ module.exports = (function() {
         },
         repos: 'https://github.com/FortAwesome/Font-Awesome.git',
         name: "font-awesome",
-        version: '4.2.0',
+        version: 'v4.2.0',
         description: 'The iconic font and CSS toolkit http://fontawesome.io',
         mapping: [
             {
