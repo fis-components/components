a498ad73571cafbc12b468918fd923de74211f41
廖学芝 <2betop.cn@gmail.com>
2014-12-09 15:14:59 +0800

    Create font-awesome.js

---
+++ modules/font-awesome.js
@@ -0,0 +1,36 @@
+'use strict';
+
+module.exports = (function() {
+    return [{
+        protocol: "github",
+        github: {
+            author: "fis-components"
+        },
+        repos: 'https://github.com/FortAwesome/Font-Awesome.git',
+        name: "font-awesome",
+        version: '4.2.0',
+        description: 'The iconic font and CSS toolkit http://fontawesome.io',
+        mapping: [
+            {
+                reg: /^\/css\/(.*?)$/,
+                release: '$1'
+            },
+            {
+                reg: /^\/scss\/(.*?)$/,
+                release: '$1'
+            },
+            {
+                reg: /^\/less\/(.*?)$/,
+                release: '$1'
+            },
+            {
+                reg: /^\/fonts\/(.*?)$/,
+                release: '$1'
+            },
+            {
+                reg: '*',
+                release: false
+            }
+        ]
+    }];
+})();
