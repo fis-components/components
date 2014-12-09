c369261be3b53fc74d43afd90599142838e02d98
廖学芝 <2betop.cn@gmail.com>
2014-12-09 23:47:17 +0800

    Update webuploader.js

--- modules/webuploader.js
+++ modules/webuploader.js
@@ -9,7 +9,7 @@ module.exports = (function() {
         name: "webuploader",
         repos: 'https://github.com/fex-team/webuploader.git',
         version: '0.1.5',
-        description: 'It's a new file uploader solution! http://fex.baidu.com/webuploader',
+        description: 'It\'s a new file uploader solution! http://fex.baidu.com/webuploader',
         build: 'npm install && npm install grunt-cli && ./node_modules/.bin/grunt dist',
         dependencies: [
             "jquery@>=1.6"
