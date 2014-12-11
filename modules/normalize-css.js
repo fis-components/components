1c758db8862ff37332fb25f3868e519c559b8d30
liaoxuezhi <liaoxuezhi@baidu.com>
2014-12-12 01:06:07 +0800

    add more

---
+++ modules/normalize-css.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = (function() {
+    return [{
+        protocol: "github",
+        github: {
+            author: "fis-components"
+        },
+        repos: 'https://github.com/necolas/normalize.css.git',
+        version: 'v2.1.3',
+        name: 'normalize-css',
+        mapping: [
+            {
+                reg: /\.min\.(js|css)$/,
+                release: false
+            },
+            {
+                reg: /^\/normalize\.css$/,
+                release: '$&'
+            },
+            {
+                reg: /^\/README\.md$/,
+                release: '$&'
+            },
+            {
+                reg: '*',
+                release: false
+            }
+        ]
+    }]
+})();
---
+++ modules/text.js
@@ -0,0 +1,32 @@
+'use strict';
+
+module.exports = (function() {
+    return [{
+        protocol: "github",
+        github: {
+            author: "fis-components"
+        },
+        repos: 'https://github.com/requirejs/text.git',
+        version: '2.0.12',
+        name: 'text',
+        main: 'text.js',
+        mapping: [
+            {
+                reg: /\.min\.(js|css)$/,
+                release: false
+            },
+            {
+                reg: /^\/text\.js$/,
+                release: '$&'
+            },
+            {
+                reg: /^\/README\.md$/,
+                release: '$&'
+            },
+            {
+                reg: '*',
+                release: false
+            }
+        ]
+    }]
+})();
