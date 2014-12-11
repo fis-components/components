46c1dca36907cb692d02b35d1cb6b0dfb05449a4
liaoxuezhi <liaoxuezhi@baidu.com>
2014-12-12 01:09:30 +0800

    add more

---
+++ modules/jquery-cookie.js
@@ -0,0 +1,35 @@
+'use strict';
+
+module.exports = (function() {
+    return [{
+        protocol: "github",
+        github: {
+            author: "fis-components"
+        },
+        repos: 'https://github.com/carhartl/jquery-cookie.git',
+        version: 'v1.4.1',
+        name: 'jquery-cookie',
+        main: 'jquery-cookie.js',
+        dependencies: [
+            "jquery@>=1.6"
+        ],
+        mapping: [
+            {
+                reg: /\.min\.(js|css)$/,
+                release: false
+            },
+            {
+                reg: /^\/src\/jquery\.cookie\.js\.js$/,
+                release: '/jquery.cookie.js'
+            },
+            {
+                reg: /^\/README\.md$/,
+                release: '$&'
+            },
+            {
+                reg: '*',
+                release: false
+            }
+        ]
+    }]
+})();
