6ca3337524dd99941ea433e1ac7bd485aed9e984
liaoxuezhi <liaoxuezhi@baidu.com>
2014-12-12 18:01:24 +0800

    add require-css

---
+++ modules/css.js
@@ -0,0 +1,32 @@
+'use strict';
+
+module.exports = (function() {
+    return [{
+        protocol: "github",
+        github: {
+            author: "fis-components"
+        },
+        repos: 'https://github.com/guybedford/require-css.git',
+        version: '0.1.8',
+        name: 'css',
+        main: 'css.js',
+        mapping: [
+            {
+                reg: /\.min\.(js|css)$/,
+                release: false
+            },
+            {
+                reg: /^\/css\.js$/,
+                release: '$&'
+            },
+            {
+                reg: /^\/README\.md$/,
+                release: '$&'
+            },
+            {
+                reg: '*',
+                release: false
+            }
+        ]
+    }]
+})();
