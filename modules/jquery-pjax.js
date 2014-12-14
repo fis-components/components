b8649f364e76f38152b439ba5574016af37dc10d
liaoxuezhi <liaoxuezhi@baidu.com>
2014-12-14 14:37:16 +0800

    add jquery-pjax

---
+++ modules/jquery-pjax.js
@@ -0,0 +1,40 @@
+'use strict';
+
+module.exports = (function() {
+    return [{
+        protocol: "github",
+        github: {
+            author: "fis-components"
+        },
+        repos: 'https://github.com/defunkt/jquery-pjax.git',
+        version: 'v1.9.4',
+        name: 'jquery-pjax',
+        main: 'jquery-pjax.js',
+        dependencies: [
+            "jquery@>=1.8"
+        ],
+        mapping: [
+            {
+                reg: /\.min\.(js|css)$/,
+                release: false
+            },
+            {
+                reg: /^\/jquery\.pjax\.js$/,
+                release: 'jquery.pjax.js'
+            },
+            {
+                reg: /^\/README\.md$/,
+                release: '$&'
+            },
+            {
+                reg: '*',
+                release: false
+            }
+        ],
+        shim: {
+            "jquery.pjax.js": {
+              "deps": ["jquery"]
+            }
+        }
+    }]
+})();
