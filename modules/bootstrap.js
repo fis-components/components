445480a37741a07fb3a2fe3e9f9fad72a6e3bdfd
liaoxuezhi <liaoxuezhi@baidu.com>
2014-12-27 13:03:45 +0800

    add bootstrap

---
+++ modules/bootstrap.js
@@ -0,0 +1,42 @@
+'use strict';
+
+module.exports = (function() {
+    return [{
+        protocol: "github",
+        github: {
+            author: "fis-components"
+        },
+        repos: 'https://github.com/twbs/bootstrap.git',
+        version: 'v3.3.1',
+        main: 'js/bootstrap.js',
+        mapping: [
+            {
+                reg: /\.min\.(js|css)$/,
+                release: false
+            },
+
+            {
+                reg: /^\/dist\/(.*)/,
+                release: '$1'
+            },
+
+            {
+                reg: /^\/README\.md$/,
+                release: '$&'
+            },
+
+            {
+                reg: '*',
+                release: false
+            }
+        ],
+        dependencies: [
+            "jquery@>=1.9.1"
+        ],
+        shim: {
+            "js/bootstrap.js": {
+              "deps": ["jquery"]
+            }
+        }
+    }];
+})();
