37af921cdb8d1b284f0ed8e7b431ff736cc62574
2betop <2betop.cn@gmail.com>
2015-01-28 23:20:47 +0800

    add jquery-ui-timepicker-addon

---
+++ modules/jquery-ui-timepicker-addon.js
@@ -0,0 +1,40 @@
+'use strict';
+
+module.exports = (function() {
+    return [{
+        repos: 'https://github.com/trentrichardson/jQuery-Timepicker-Addon.git',
+        version: 'v1.5.0',
+        name: 'jquery-ui-timepicker-addon',
+        main: 'jquery-ui-timepicker-addon.js',
+        dependencies: [
+            "jquery@>=1.6"
+        ],
+        mapping: [
+            {
+                reg: /\.min\.(js|css)$/,
+                release: false
+            },
+            {
+                reg: /^\/dist\/(.*\.(?:js|css))$/,
+                release: '$1'
+            },
+            {
+                reg: /^\/README\.md$/,
+                release: '$&'
+            },
+            {
+                reg: '*',
+                release: false
+            }
+        ],
+        shim: {
+            "jquery-ui-sliderAccess.js": {
+              "deps": ["jquery"]
+            },
+
+            "jquery-ui-timepicker-addon.js": {
+              "deps": ["jquery", "./jquery-ui-sliderAccess"]
+            }
+        }
+    }]
+})();
