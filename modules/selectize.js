6596c2bf8cde4529b67926e758946395034d5cd1
cos800 <thisky@qq.com>
2015-01-28 14:54:46 +0800

    修正selectize.js 有依赖 jquery

--- modules/selectize.js
+++ modules/selectize.js
@@ -5,20 +5,27 @@ module.exports = (function() {
         repos: 'https://github.com/brianreavis/selectize.js.git',
         version: 'v0.11.2',
         main: 'selectize.js',
-
+        dependencies: [
+            "jquery@>=1.7"
+        ],
         mapping: [
-			{
-				reg: /^\/dist\/js\/standalone\/selectize\.js$/,
-				release: 'selectize.js'
-			},
-			{
-				reg: /^\/dist\/css\/(.+\.css)$/,
-				release: '$1'
-			},
+            {
+                reg: /^\/dist\/js\/standalone\/selectize\.js$/,
+                release: 'selectize.js'
+            },
+            {
+                reg: /^\/dist\/css\/(.+\.css)$/,
+                release: '$1'
+            },
             {
                 reg: '*',
                 release: false
             }
-        ]
+        ],
+        shim: {
+            "selectize.js": {
+                "deps": ["jquery"]
+            }
+        }
     }];
 })();
