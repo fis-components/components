fa8432e7a80946a5179b32aea70646c3cfa4d224
2betop <2betop.cn@gmail.com>
2015-02-03 13:26:13 +0800

    update jquery-cookie

--- modules/jquery-cookie.js
+++ modules/jquery-cookie.js
@@ -15,7 +15,7 @@ module.exports = (function() {
                 release: false
             },
             {
-                reg: 'src/jquery.cookie.js',
+                reg: 'jquery.cookie.js',
                 release: 'jquery.cookie.js'
             },
             {
