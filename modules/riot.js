7f278b0689de7ffb714a618ec64611c911a3227a
xiangshouding <fansekey@gmail.com>
2015-03-09 11:59:14 +0800

    update

--- modules/riot.js
+++ modules/riot.js
@@ -9,7 +9,7 @@ module.exports = (function() {
         main: 'riot.js',
         mapping: [
             {
-                reg: 'riot.js',
+                reg: /^\/riot\.js$/,
                 release: '$&'
             },
             {
