aae6bc1676aa8c391e3b36403521b16aa1cb00d3
TZ <atian25@qq.com>
2015-03-16 10:12:53 +0800

    lodash
    Signed-off-by: TZ <atian25@qq.com>

---
+++ modules/lodash.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = (function() {
+    return [{
+        repos: 'https://github.com/lodash/lodash',
+        version: '3.5.0',
+        description: 'A JavaScript utility library delivering consistency, modularity, performance, & extras. ',
+        name: 'lodash',
+        main: 'lodash.js',
+        mapping: [
+            {
+                reg: /^\/lodash\.js$/,
+                release: '$&'
+            },
+            {
+                reg: /^\/README\.md$/,
+                release: '$&'
+            },
+            {
+                reg: '*',
+                release: false
+            }
+        ]
+    }]
+})();
