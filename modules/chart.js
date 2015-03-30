c6ec4100ce15c3c50d4abd6b8aa163d66fb5197f
hefangshi <hefangshi@gmail.com>
2015-03-30 13:34:22 +0800

    Update chart.js

--- modules/chart.js
+++ modules/chart.js
@@ -5,7 +5,7 @@ module.exports = (function() {
         repos: 'https://github.com/nnnick/Chart.js',
         version: 'v1.0.2',
         description: 'Simple HTML5 Charts using the <canvas> tag',
-        name: 'chart.js',
+        name: 'chart',
         main: 'Chart.js',
         mapping: [
             {
