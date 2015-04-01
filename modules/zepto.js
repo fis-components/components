451782371fbb1f22220db7ba2df5b0a4b10f6717
2betop <liaoxuezhi@icloud.com>
2015-04-01 18:08:56 +0800

    optimize zepto

--- modules/zepto.js
+++ modules/zepto.js
@@ -5,15 +5,10 @@ module.exports = (function() {
         repos: 'https://github.com/madrobby/zepto.git',
         version: 'v1.1.4',
         description: 'zepto',
-        build: 'npm install && npm run-script dist',
-        main: 'zepto.js',
+        main: 'main.js',
         mapping: [
             {
-                reg: /\.min\.(js|css)$/,
-                release: false
-            },
-            {
-                reg: /^\/dist\/(.*\.js)/,
+                reg: /^\/src\/(.*)$/,
                 release: '$1'
             },
             {
@@ -27,8 +22,379 @@ module.exports = (function() {
         ],
         shim: {
             'zepto.js': {
-
                 "exports": ["Zepto"]
+            },
+
+            'ajax.js': {
+                "deps": ["./zepto.js"],
+                "vars": ["Zepto"],
+                "exports": ["Zepto"]
+            },
+
+            'assets.js': {
+                "deps": ["./zepto.js"],
+                "vars": ["Zepto"],
+                "exports": ["Zepto"]
+            },
+
+            'callbacks.js': {
+                "deps": ["./zepto.js"],
+                "vars": ["Zepto"],
+                "exports": ["Zepto"]
+            },
+
+            'data.js': {
+                "deps": ["./zepto.js"],
+                "vars": ["Zepto"],
+                "exports": ["Zepto"]
+            },
+
+            'deferred.js': {
+                "deps": ["./zepto.js", "./callbacks.js"],
+                "vars": ["Zepto"],
+                "exports": ["Zepto"]
+            },
+
+            'detect.js': {
+                "deps": ["./zepto.js"],
+                "vars": ["Zepto"],
+                "exports": ["Zepto"]
+            },
+
+            'event.js': {
+                "deps": ["./zepto.js"],
+                "vars": ["Zepto"],
+                "exports": ["Zepto"]
+            },
+
+            'form.js': {
+                "deps": ["./zepto.js"],
+                "vars": ["Zepto"],
+                "exports": ["Zepto"]
+            },
+
+            'fx.js': {
+                "deps": ["./zepto.js"],
+                "vars": ["Zepto"],
+                "exports": ["Zepto"]
+            },
+
+            'fx_methods.js': {
+                "deps": ["./zepto.js", "./fx.js"],
+                "vars": ["Zepto"],
+                "exports": ["Zepto"]
+            },
+
+            'guesture.js': {
+                "deps": ["./zepto.js"],
+                "vars": ["Zepto"],
+                "exports": ["Zepto"]
+            },
+
+            'ie.js': {
+                "deps": ["./zepto.js"],
+                "vars": ["Zepto"],
+                "exports": ["Zepto"]
+            },
+
+            'ios3.js': {
+                "deps": ["./zepto.js"],
+                "vars": ["Zepto"],
+                "exports": ["Zepto"]
+            },
+
+            'selector.js': {
+                "deps": ["./zepto.js"],
+                "vars": ["Zepto"],
+                "exports": ["Zepto"]
+            },
+
+            'stack.js': {
+                "deps": ["./zepto.js"],
+                "vars": ["Zepto"],
+                "exports": ["Zepto"]
+            },
+
+            'touch.js': {
+                "deps": ["./zepto.js"],
+                "vars": ["Zepto"],
+                "exports": ["Zepto"]
+            },
+
+            'main.js': {
+                "content": [
+                    "require('./event.js');",
+                    "require('./ajax.js');",
+                    "require('./form.js');",
+                    "require('./ie.js');"
+                ].join('\n'),
+                "exports": "require('./zepto.js')"
+            }
+        }
+    },
+
+
+    {
+        repos: 'https://github.com/madrobby/zepto.git',
+        version: 'v1.1.5',
+        description: 'zepto',
+        main: 'main.js',
+        mapping: [
+            {
+                reg: /^\/src\/(.*)$/,
+                release: '$1'
+            },
+            {
+                reg: /^\/README\.md$/,
+                release: '$&'
+            },
+            {
+                reg: '*',
+                release: false
+            }
+        ],
+        shim: {
+            'zepto.js': {
+                "exports": ["Zepto"]
+            },
+
+            'ajax.js': {
+                "deps": ["./zepto.js"],
+                "vars": ["Zepto"],
+                "exports": ["Zepto"]
+            },
+
+            'assets.js': {
+                "deps": ["./zepto.js"],
+                "vars": ["Zepto"],
+                "exports": ["Zepto"]
+            },
+
+            'callbacks.js': {
+                "deps": ["./zepto.js"],
+                "vars": ["Zepto"],
+                "exports": ["Zepto"]
+            },
+
+            'data.js': {
+                "deps": ["./zepto.js"],
+                "vars": ["Zepto"],
+                "exports": ["Zepto"]
+            },
+
+            'deferred.js': {
+                "deps": ["./zepto.js", "./callbacks.js"],
+                "vars": ["Zepto"],
+                "exports": ["Zepto"]
+            },
+
+            'detect.js': {
+                "deps": ["./zepto.js"],
+                "vars": ["Zepto"],
+                "exports": ["Zepto"]
+            },
+
+            'event.js': {
+                "deps": ["./zepto.js"],
+                "vars": ["Zepto"],
+                "exports": ["Zepto"]
+            },
+
+            'form.js': {
+                "deps": ["./zepto.js"],
+                "vars": ["Zepto"],
+                "exports": ["Zepto"]
+            },
+
+            'fx.js': {
+                "deps": ["./zepto.js"],
+                "vars": ["Zepto"],
+                "exports": ["Zepto"]
+            },
+
+            'fx_methods.js': {
+                "deps": ["./zepto.js", "./fx.js"],
+                "vars": ["Zepto"],
+                "exports": ["Zepto"]
+            },
+
+            'guesture.js': {
+                "deps": ["./zepto.js"],
+                "vars": ["Zepto"],
+                "exports": ["Zepto"]
+            },
+
+            'ie.js': {
+                "deps": ["./zepto.js"],
+                "vars": ["Zepto"],
+                "exports": ["Zepto"]
+            },
+
+            'ios3.js': {
+                "deps": ["./zepto.js"],
+                "vars": ["Zepto"],
+                "exports": ["Zepto"]
+            },
+
+            'selector.js': {
+                "deps": ["./zepto.js"],
+                "vars": ["Zepto"],
+                "exports": ["Zepto"]
+            },
+
+            'stack.js': {
+                "deps": ["./zepto.js"],
+                "vars": ["Zepto"],
+                "exports": ["Zepto"]
+            },
+
+            'touch.js': {
+                "deps": ["./zepto.js"],
+                "vars": ["Zepto"],
+                "exports": ["Zepto"]
+            },
+
+            'main.js': {
+                "content": [
+                    "require('./event.js');",
+                    "require('./ajax.js');",
+                    "require('./form.js');",
+                    "require('./ie.js');"
+                ].join('\n'),
+                "exports": "require('./zepto.js')"
+            }
+        }
+    },
+
+
+    {
+        repos: 'https://github.com/madrobby/zepto.git',
+        version: 'v1.1.6',
+        description: 'zepto',
+        main: 'main.js',
+        mapping: [
+            {
+                reg: /^\/src\/(.*)$/,
+                release: '$1'
+            },
+            {
+                reg: /^\/README\.md$/,
+                release: '$&'
+            },
+            {
+                reg: '*',
+                release: false
+            }
+        ],
+        shim: {
+            'zepto.js': {
+                "exports": ["Zepto"]
+            },
+
+            'ajax.js': {
+                "deps": ["./zepto.js"],
+                "vars": ["Zepto"],
+                "exports": ["Zepto"]
+            },
+
+            'assets.js': {
+                "deps": ["./zepto.js"],
+                "vars": ["Zepto"],
+                "exports": ["Zepto"]
+            },
+
+            'callbacks.js': {
+                "deps": ["./zepto.js"],
+                "vars": ["Zepto"],
+                "exports": ["Zepto"]
+            },
+
+            'data.js': {
+                "deps": ["./zepto.js"],
+                "vars": ["Zepto"],
+                "exports": ["Zepto"]
+            },
+
+            'deferred.js': {
+                "deps": ["./zepto.js", "./callbacks.js"],
+                "vars": ["Zepto"],
+                "exports": ["Zepto"]
+            },
+
+            'detect.js': {
+                "deps": ["./zepto.js"],
+                "vars": ["Zepto"],
+                "exports": ["Zepto"]
+            },
+
+            'event.js': {
+                "deps": ["./zepto.js"],
+                "vars": ["Zepto"],
+                "exports": ["Zepto"]
+            },
+
+            'form.js': {
+                "deps": ["./zepto.js"],
+                "vars": ["Zepto"],
+                "exports": ["Zepto"]
+            },
+
+            'fx.js': {
+                "deps": ["./zepto.js"],
+                "vars": ["Zepto"],
+                "exports": ["Zepto"]
+            },
+
+            'fx_methods.js': {
+                "deps": ["./zepto.js", "./fx.js"],
+                "vars": ["Zepto"],
+                "exports": ["Zepto"]
+            },
+
+            'guesture.js': {
+                "deps": ["./zepto.js"],
+                "vars": ["Zepto"],
+                "exports": ["Zepto"]
+            },
+
+            'ie.js': {
+                "deps": ["./zepto.js"],
+                "vars": ["Zepto"],
+                "exports": ["Zepto"]
+            },
+
+            'ios3.js': {
+                "deps": ["./zepto.js"],
+                "vars": ["Zepto"],
+                "exports": ["Zepto"]
+            },
+
+            'selector.js': {
+                "deps": ["./zepto.js"],
+                "vars": ["Zepto"],
+                "exports": ["Zepto"]
+            },
+
+            'stack.js': {
+                "deps": ["./zepto.js"],
+                "vars": ["Zepto"],
+                "exports": ["Zepto"]
+            },
+
+            'touch.js': {
+                "deps": ["./zepto.js"],
+                "vars": ["Zepto"],
+                "exports": ["Zepto"]
+            },
+
+            'main.js': {
+                "content": [
+                    "require('./event.js');",
+                    "require('./ajax.js');",
+                    "require('./form.js');",
+                    "require('./ie.js');"
+                ].join('\n'),
+                "exports": "require('./zepto.js')"
             }
         }
     }]
