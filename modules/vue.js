af708e985104464faf69ac1857594509250a8736
BelinChung <belin@hiliaox.com>
2015-04-02 15:07:22 +0800

    Update modules/vue.js

--- modules/vue.js
+++ modules/vue.js
@@ -9,7 +9,7 @@ module.exports = (function() {
         main: "src/vue.js",
         mapping: [
             {
-                reg: /^\/src\/(.*)$/,
+                reg: /^\/dist\/(.*\.js)/,
                 release: '$&'
             },
             {
