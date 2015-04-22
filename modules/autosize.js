762c8791ec7fb35391573843cb7947394dbb2bea
BelinChung <belinchung@gmail.com>
2015-04-22 15:08:30 +0800

    autosize.js

---
+++ modules/autosize.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = (function() {
+    return [{
+        repos: 'https://github.com/jackmoore/autosize',
+        version: '3.0.0',
+        description: 'Autosize is a small, stand-alone script to automatically adjust textarea height to fit text.http://www.jacklmoore.com/autosize/',
+        name: 'autosize',
+        main: 'autosize.js',
+        mapping: [
+            {
+                reg: '/dest/autosize.js',
+                release: 'autosize.js'
+            },
+            {
+                reg: /^\/readme\.md$/,
+                release: '$&'
+            },
+            {
+                reg: '*',
+                release: false
+            }
+        ]
+    }]
+})();
