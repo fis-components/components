5019b9c91bfb8d0fa9b545b49d8982d4afcc6d44
2betop <liaoxuezhi@icloud.com>
2015-05-11 09:51:49 +0800

    update

--- modules/vue-validator.js
+++ modules/vue-validator.js
@@ -3,6 +3,56 @@
 module.exports = (function() {
     return [{
         repos: 'https://github.com/vuejs/vue-validator',
+        version: 'v1.0.2',
+        description: 'Validator component for VueJS',
+        name: 'vue-validator',
+        main: 'vue-validator.js',
+        dependencies: [
+            "vue@>=0.11.2"
+        ],
+        mapping: [
+            {
+                reg: /^\/dist\/(vue-validator\.js)/,
+                release: '$1'
+            },
+            {
+                reg: /^\/README\.md$/,
+                release: '$&'
+            },
+            {
+                reg: '*',
+                release: false
+            }
+        ]
+    },
+
+    {
+        repos: 'https://github.com/vuejs/vue-validator',
+        version: 'v1.0.3',
+        description: 'Validator component for VueJS',
+        name: 'vue-validator',
+        main: 'vue-validator.js',
+        dependencies: [
+            "vue@>=0.11.2"
+        ],
+        mapping: [
+            {
+                reg: /^\/dist\/(vue-validator\.js)/,
+                release: '$1'
+            },
+            {
+                reg: /^\/README\.md$/,
+                release: '$&'
+            },
+            {
+                reg: '*',
+                release: false
+            }
+        ]
+    },
+
+    {
+        repos: 'https://github.com/vuejs/vue-validator',
         version: 'v1.0.4',
         description: 'Validator component for VueJS',
         name: 'vue-validator',
--- modules/vue.js
+++ modules/vue.js
@@ -3,6 +3,50 @@
 module.exports = (function() {
     return [{
         repos: 'https://github.com/yyx990803/vue',
+        version: '0.11.8',
+        description: 'Simple, Fast & Composable MVVM for building interative interfaces',
+        name: "vue",
+        main: "vue.js",
+        mapping: [
+            {
+                reg: /^\/dist\/(vue\.js)/,
+                release: '$1'
+            },
+            {
+                reg: /^\/README\.md$/,
+                release: '$&'
+            },
+            {
+                reg: '*',
+                release: false
+            }
+        ]
+    },
+
+    {
+        repos: 'https://github.com/yyx990803/vue',
+        version: '0.11.9',
+        description: 'Simple, Fast & Composable MVVM for building interative interfaces',
+        name: "vue",
+        main: "vue.js",
+        mapping: [
+            {
+                reg: /^\/dist\/(vue\.js)/,
+                release: '$1'
+            },
+            {
+                reg: /^\/README\.md$/,
+                release: '$&'
+            },
+            {
+                reg: '*',
+                release: false
+            }
+        ]
+    },
+
+    {
+        repos: 'https://github.com/yyx990803/vue',
         version: '0.11.10',
         description: 'Simple, Fast & Composable MVVM for building interative interfaces',
         name: "vue",
