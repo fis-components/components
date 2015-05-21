52524f2b524851eb1af57dac3a4a1593b9d4c1c3
BelinChung <belinchung@gmail.com>
2015-05-21 09:35:06 +0800

    add crropper.js

---
+++ modules/cropper.js
@@ -0,0 +1,38 @@
+'use strict';
+
+module.exports = (function() {
+    return [{
+        repos: 'https://github.com/fengyuanchen/cropper',
+        version: 'v0.9.3',
+        description: 'A simple jQuery image cropping plugin. http://fengyuanchen.github.io/cropper',
+        name: 'cropper',
+        main: 'cropper.js',
+        dependencies: [
+            "jquery@>=1.7.0"
+        ],
+        mapping: [
+            {
+                reg: '/dist/cropper.js',
+                release: 'cropper.js'
+            },
+            {
+                reg: '/dist/cropper.css',
+                release: 'cropper.css'
+            },
+            {
+                reg: /^\/README\.md$/,
+                release: '$&'
+            },
+            {
+                reg: '*',
+                release: false
+            }
+        ],
+        shim: {
+            'cropper.js': {
+                deps: ["jquery"],
+                vars: ["jQuery"]
+            }
+        }
+    }]
+})();
