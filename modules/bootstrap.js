d6c1ede12cc9e022cca5ad23012182fc8881c642
2betop <liaoxuezhi@icloud.com>
2015-05-28 00:33:58 +0800

    update

--- modules/bootstrap.js
+++ modules/bootstrap.js
@@ -102,7 +102,8 @@ module.exports = (function() {
 
             "popover.js": {
               "deps": ["jquery"],
-              "vars": ["jQuery"]
+              "vars": ["jQuery"],
+              "deps": ["./tooltip.js"]
             },
 
             "scrollspy.js": {
