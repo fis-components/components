563ab0e3c0bbb9fdc160f13a7dd9d037715ca915
TZ | 天猪 <atian25@qq.com>
2015-05-27 10:21:44 +0800

    Update angular-resource.js

--- modules/angular-resource.js
+++ modules/angular-resource.js
@@ -3,7 +3,7 @@
 module.exports = (function() {
     return [{
         repos: 'https://github.com/angular/bower-angular-resource',
-        version: 'v1.3.14',
+        version: 'v1.4.0',
         description: 'HTML enhanced for web apps http://angularjs.org',
         name: 'angular-resource',
         main: 'angular-resource.js',
