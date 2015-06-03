b5f6c8e0e847891b3f53117ed72a11d178db2d71
2betop <liaoxuezhi@icloud.com>
2015-06-03 15:13:26 +0800

    add boostui

---
+++ modules/boostui.js
@@ -0,0 +1,46 @@
+'use strict';
+
+module.exports = (function() {
+    return [{
+        repos: 'https://github.com/Clouda-team/boostui.git',
+        tag: 'master',
+        version: '1.0.0',
+        main: 'boost.js',
+        mapping: [
+            {
+                reg: /\.min\.(js|css)$/,
+                release: false
+            },
+
+            {
+                reg: /^\/dist\/js\/npm\.js$/,
+                release: false
+            },
+
+            {
+                reg: /^\/dist\/js\/(.*\.js)$/,
+                release: '$1'
+            },
+
+            {
+                reg: /^\/dist\/(.*)$/,
+                release: '$1'
+            },
+
+            {
+                reg: /^\/js\/([^\/]*\.js)$/,
+                release: '$1'
+            },
+
+            {
+                reg: /^\/README\.md$/,
+                release: '$&'
+            },
+
+            {
+                reg: '*',
+                release: false
+            }
+        ]
+    }];
+})();
