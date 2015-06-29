71b64d42b35151d191ab2b0ee1023b8e279fb276
jiexuangao <jiexuangao@gmail.com>
2015-06-29 18:24:59 +0800

    update rosetta

--- modules/rosetta.js
+++ modules/rosetta.js
@@ -5,16 +5,11 @@ module.exports = (function() {
         repos: 'https://github.com/jiexuangao/rosetta.git',
         version: '1.0.1',
         main: 'Rosetta.js',
-        tag: 'v1.0.1',
+        tag: '1.0.1',
         mapping: [
             {
                 reg: /^\/dist\/((Rosetta|Rosetta-ie)\.(css|js))$/ig,
                 release: '$1'
-            },
-
-            {
-                reg: /^\/README\.md$/,
-                release: '$&'
             },
 
             {
