6db305de28e4a6af14a2d1d88afee8202cb05def
jiexuangao <jiexuangao@gmail.com>
2015-06-29 19:27:50 +0800

    add all versions

--- modules/rosetta.js
+++ modules/rosetta.js
@@ -1,21 +1,30 @@
 'use strict';
 
 module.exports = (function() {
-    return [{
-        repos: 'https://github.com/jiexuangao/rosetta.git',
-        version: '1.0.1',
-        main: 'Rosetta.js',
-        tag: '1.0.1',
-        mapping: [
-            {
-                reg: /^\/dist\/((Rosetta|Rosetta-ie)\.(css|js))$/ig,
-                release: '$1'
-            },
+    return [
+        {
+            repos: 'https://github.com/jiexuangao/rosetta.git',
+            version: '1.0.1',
+            main: 'Rosetta.js',
+            tag: '1.0.1',
+            mapping: [
+                {
+                    reg: /^\/dist\/((Rosetta|Rosetta-ie)\.(css|js))$/ig,
+                    release: '$1'
+                },
 
-            {
-                reg: '*',
-                release: false
-            }
-        ]
-    }];
+                {
+                    reg: '*',
+                    release: false
+                }
+            ]
+        },
+
+        {
+            version: '1.0.0',
+            tag: 'master',
+            extend: '1.0.1'
+        }
+
+    ];
 })();
