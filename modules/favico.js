103139c166dbb0280558809e077cabad6d2b26d0
BelinChung <belinchung@gmail.com>
2015-07-15 18:22:35 +0800

    add favico.js

---
+++ modules/favico.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = (function() {
+    return [{
+        repos: 'https://github.com/ejci/favico.js',
+        version: 'master',
+        description: 'Make use of your favicon with badges, images or videos http://lab.ejci.net/favico.js/',
+        name: 'favico',
+        main: 'favico.js',
+        mapping: [
+            {
+                reg: /^\/favico\.js$/,
+                release: '$&'
+            },
+            {
+                reg: /^\/readme\.md$/,
+                release: '$&'
+            },
+            {
+                reg: '*',
+                release: false
+            }
+        ]
+    }]
+})();
