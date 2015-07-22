257422d9eeab503c7fd8a132d32c078a5dec515f
2betop <liaoxuezhi@icloud.com>
2015-07-22 11:02:36 +0800

    update jquery

--- bosSync.sh
+++ bosSync.sh
@@ -9,7 +9,7 @@ mkdir -p temp
 remote="https://codeload.github.com/fis-components/${repos}/tar.gz/${version}"
 filename="temp/${repos}@${version}_all.tar.gz"
 echo "Downloading ${remote}"
-curl -o $filename $remote
+curl -o "${ROOT}/${filename}" $remote
 if [ "$?" != "0" ]; then
     exit 1
 fi
@@ -17,7 +17,7 @@ fi
 info="https://raw.githubusercontent.com/fis-components/${repos}/${version}/component.json"
 infofile="temp/${repos}@${version}_component.json"
 echo "Downloading ${info}"
-curl -o $infofile $info
+curl -o "${ROOT}/${infofile}" $info
 if [ "$?" != "0" ]; then
     exit 1
 fi
@@ -28,7 +28,7 @@ sk="${BOS_SK}"
 versions=$(git ls-remote --tags --refs --heads https://github.com/fis-components/${repos}.git  | awk -F/ '{print $3}')
 versionsfile="temp/${repos}_versions.txt"
 echo $versions
-echo $versions > $versionsfile
+echo $versions > "${ROOT}/${versionsfile}"
 
 #php -v
 php $ROOT/bos/sync.php "${ak}" "${sk}" $repos $version $filename $infofile $versionsfile
