34083ffbfcb1522640642d04060eb5567dfd1479
2betop <liaoxuezhi@icloud.com>
2015-07-22 11:08:41 +0800

    update jquery

--- bosSync.sh
+++ bosSync.sh
@@ -5,11 +5,13 @@ ROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 repos=$1
 version=$2
 
+rm -rf temp
 mkdir -p temp
 remote="https://codeload.github.com/fis-components/${repos}/tar.gz/${version}"
 filename="temp/${repos}@${version}_all.tar.gz"
 echo "Downloading ${remote}"
-curl -o "${ROOT}/${filename}" $remote
+echo "curl -o ${ROOT}/${filename} $remote"
+curl -o ${ROOT}/${filename} $remote
 if [ "$?" != "0" ]; then
     exit 1
 fi
@@ -17,7 +19,8 @@ fi
 info="https://raw.githubusercontent.com/fis-components/${repos}/${version}/component.json"
 infofile="temp/${repos}@${version}_component.json"
 echo "Downloading ${info}"
-curl -o "${ROOT}/${infofile}" $info
+echo "curl -o ${ROOT}/${infofile} $info"
+curl -o ${ROOT}/${infofile} $info
 if [ "$?" != "0" ]; then
     exit 1
 fi
@@ -28,7 +31,7 @@ sk="${BOS_SK}"
 versions=$(git ls-remote --tags --refs --heads https://github.com/fis-components/${repos}.git  | awk -F/ '{print $3}')
 versionsfile="temp/${repos}_versions.txt"
 echo $versions
-echo $versions > "${ROOT}/${versionsfile}"
+echo $versions > ${ROOT}/${versionsfile}
 
 #php -v
 php $ROOT/bos/sync.php "${ak}" "${sk}" $repos $version $filename $infofile $versionsfile
