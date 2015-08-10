a6592ee5fd7314884100453b1624c98aa789f553
2betop <liaoxuezhi@icloud.com>
2015-08-10 10:26:40 +0800

    fix echarts

--- modules/echarts.js
+++ modules/echarts.js
@@ -15,6 +15,10 @@ module.exports = (function() {
                 release: false
             },
             {
+                reg: /\/echarts\/util\/mapData\/rawData\/.*$/i,
+                release: false
+            },
+            {
                 reg: /^\/src\/(.*)$/,
                 release: '$1'
             },
