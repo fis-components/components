b6adefd5acc943c3792c2f84a4e04925b0acfd54
jinx151 <634644441@qq.com>
2015-08-27 13:45:18 +0800

    Update jquery-parallax.js

--- modules/jquery-parallax.js
+++ modules/jquery-parallax.js
@@ -30,7 +30,7 @@ module.exports = (function() {
         ],
 
         shim: {
-            "jquery. parallax.js": {
+            "jquery.parallax.js": {
               "deps": ["jquery"],
               "vars": ["jQuery"]
             }
