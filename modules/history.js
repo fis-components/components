819a8f5eff2801c3615ce6785ca9174db4fd4213
2betop <liaoxuezhi@icloud.com>
2015-11-21 01:50:22 +0800

    update

--- modules/history.js
+++ modules/history.js
@@ -6,22 +6,16 @@ module.exports = (function() {
             repos: 'https://github.com/rackt/history.git',
             version: 'v1.13.1',
             name: 'history',
-            main: 'index.js',
+            main: 'history.js',
             build: 'rm package.json && npm install --prefix . history@1.13.1',
-            dependencies: [
-                "invariant@^2.0.0",
-                "deep-equal@^1.0.0",
-                "qs@^4.0.0",
-                "warning@^2.0.0"
-            ],
             mapping: [
                 {
                     reg: /\bmin\b/i,
                     release: false
                 },
                 {
-                    reg: /^\/node_modules\/history\/lib\/(.*)$/,
-                    release: '$1'
+                    reg: /^\/node_modules\/history\/umd\/(History\.js)$/,
+                    release: 'history.js'
                 },
                 {
                     reg: /^\/README\.md$/i,
