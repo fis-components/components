1fb4abc9fe944dc222ef5fc1bee7e30358d34b55
2betop <liaoxuezhi@icloud.com>
2015-11-24 22:22:04 +0800

    add more

--- modules/react-notification-system.js
+++ modules/react-notification-system.js
@@ -4,10 +4,10 @@ module.exports = (function() {
     return [
         {
             repos: 'https://github.com/igorprado/react-notification-system.git',
-            version: '0.2.6',
+            version: '0.2.4',
             name: 'react-notification-system',
-            main: 'NotificationSystem.js',
-            build: 'rm package.json && npm install --prefix . react-notification-system@0.2.6',
+            main: 'notification-system.js',
+            build: 'rm package.json && npm install --prefix . react-notification-system@0.2.4',
             dependencies: [
                 "object-assign@^4.0.1"
             ],
