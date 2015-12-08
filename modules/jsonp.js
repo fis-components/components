5f69298d38a2e91c5b7e161d947ca29028899bf5
2betop <liaoxuezhi@icloud.com>
2015-12-08 14:16:59 +0800

    add jsonp

---
+++ modules/jsonp.js
@@ -0,0 +1,44 @@
+module.exports = (function() {
+  return [{
+    repos: "https://github.com/webmodules/jsonp.git",
+    version: "0.2.0",
+    description: "A simple JSONP implementation",
+    name: "jsonp",
+    main: "index.js",
+    dependencies: [
+    ],
+    mapping: [
+        
+        {
+            reg: /^\/index\.js$/,
+            release: '$&'
+        },
+        {
+            reg: /^\/README\.md$/,
+            release: '$&'
+        },
+        {
+            reg: "*",
+            release: false
+        }
+    ],
+
+
+    // 去掉 debug 信息
+    shim: {
+        "index.js": {
+            "replace": [
+                {
+                    from: "var debug = require('debug')('jsonp');"
+                    to: ""
+                },
+
+                {
+                    from: "debug('jsonp req \"%s\"', url);"
+                    to: ""
+                }
+            ]
+        }
+    }
+  }]
+})();
