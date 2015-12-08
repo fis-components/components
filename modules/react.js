c5931c8c6a4ac500db3b43d2037db19b74ee8d20
2betop <liaoxuezhi@icloud.com>
2015-12-08 18:17:52 +0800

    add components hook

--- modules/react.js
+++ modules/react.js
@@ -24,17 +24,57 @@ module.exports = (function() {
                     release: '$&'
                 },
                 {
+                    reg: /^\/package\.json$/i,
+                    release: '.hook.js'
+                },
+                {
                     reg: '*',
                     release: false
                 }
             ],
-            umd2commonjs: false
+            umd2commonjs: false,
+            hook: '.hook.js',
+            shim: {
+                '.hook.js': {
+                    content: [
+                        "var path = require('path');",
+"",
+"module.exports = function(json, files) {",
+"  var hasAddons = false;",
+"",
+"  Object.keys(files).every(function(subpath) {",
+"    var cName = path.basename(path.dirname(subpath));",
+"",
+"    // some thing like `react-addons-create-fragment`",
+"    if (cName.substring(0, 13) === 'react-addons-') {",
+"      hasAddons = true;",
+"      return false;",
+"    }",
+"",
+"    return true;",
+"  });",
+"",
+"  // 自动切换到 react-with-addons 如果依赖列表中有 react-addons 的话。",
+"  // 实在不想再把 react 给拆了。",
+"  if (hasAddons) {",
+"    json.main = 'react-with-addons.js';",
+"  }",
+"};"
+                    ].join('\n')
+                }
+            }
         },
 
         {
             extend: 'v0.13.3',
             version: 'v0.14.2',
             build: 'rm package.json && npm install --prefix . react@0.14.2',
+        },
+
+        {
+            extend: 'v0.13.3',
+            version: 'v0.14.3',
+            build: 'rm package.json && npm install --prefix . react@0.14.3',
         }
     ]
 })();
