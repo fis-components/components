e4f06e579c56f2c31a40ddf5c35e32dc44358442
2betop <liaoxuezhi@icloud.com>
2015-12-09 09:56:27 +0800

    add simple-line-icons

---
+++ modules/simple-line-icons.js
@@ -0,0 +1,41 @@
+'use strict';
+
+module.exports = (function() {
+    return [{
+        repos: 'https://github.com/thesabbir/simple-line-icons.git',
+        name: "simple-line-icons",
+        version: 'v0.1.2',
+        main: 'css/simple-line-icons.css',
+        description: 'Simple Line Icons Package https://thesabbir.github.io/simple-line-icons',
+        mapping: [
+            {
+                reg: /\.min\.(js|css)$/,
+                release: false
+            },
+            {
+                reg: /^\/css\/(.*?)$/,
+                release: '$&'
+            },
+            {
+                reg: /^\/scss\/(.*?)$/,
+                release: '$&'
+            },
+            {
+                reg: /^\/less\/(.*?)$/,
+                release: '$&'
+            },
+            {
+                reg: /^\/fonts\/(.*?)$/,
+                release: '$&'
+            },
+            {
+                reg: /^\/readme.md$/i,
+                release: '$&'
+            },
+            {
+                reg: '*',
+                release: false
+            }
+        ]
+    }];
+})();
