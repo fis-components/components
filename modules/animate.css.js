a8b5cdc09a62eb6cd91b05ec4f5c2d99f930dcb9
2betop <liaoxuezhi@icloud.com>
2015-12-09 10:04:14 +0800

    add more

---
+++ modules/animate.css.js
@@ -0,0 +1,28 @@
+'use strict';
+
+module.exports = (function() {
+    return [{
+        repos: 'https://github.com/daneden/animate.css.git',
+        name: "animate.css",
+        version: 'v3.1.0',
+        description: 'A cross-browser library of CSS animations. As easy to use as an easy thing. http://daneden.github.io/animate.css',
+        mapping: [
+            {
+                reg: /\.min\.(js|css)$/,
+                release: false
+            },
+            {
+                reg: /^\/animate\.css$/,
+                release: '$&'
+            },
+            {
+                reg: /^\/readme\.md$/i,
+                release: '$&'
+            },
+            {
+                reg: '*',
+                release: false
+            }
+        ]
+    }];
+})();
--- modules/simple-line-icons.js
+++ modules/simple-line-icons.js
@@ -29,7 +29,7 @@ module.exports = (function() {
                 release: '$&'
             },
             {
-                reg: /^\/readme.md$/i,
+                reg: /^\/readme\.md$/i,
                 release: '$&'
             },
             {
