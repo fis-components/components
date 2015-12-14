415cc97d0cbe6aac3e8301ed6cb6c56e805d8761
liaoxuezhi <liaoxuezhi@icloud.com>
2015-12-14 11:39:32 +0800

    Update redux-simple-router.js

--- modules/redux-simple-router.js
+++ modules/redux-simple-router.js
@@ -5,6 +5,7 @@ module.exports = (function() {
         {
             repos: 'https://github.com/jlongster/redux-simple-router.git',
             version: '0.0.8',
+            tag: 'master',
             name: 'redux-simple-router',
             main: 'lib/index.js',
             build: 'rm package.json && npm install --prefix . redux-simple-router@0.0.8',
@@ -26,6 +27,18 @@ module.exports = (function() {
                     release: false
                 }
             ]
+        },
+        
+        {
+            version: '0.0.10',
+            build: 'rm package.json && npm install --prefix . redux-simple-router@0.0.10',
+            extend: '0.0.8'
+        },
+        
+        {
+            version: '1.0.0',
+            build: 'rm package.json && npm install --prefix . redux-simple-router@1.0.0',
+            extend: '0.0.8'
         }
     ]
 })();
