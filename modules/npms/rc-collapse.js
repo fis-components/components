8c66484ab1c040f2a6c062215caad9c3d82694ad
2betop <liaoxuezhi@icloud.com>
2015-12-17 02:39:16 +0800

    add more

---
+++ modules/npms/rc-collapse.js
@@ -0,0 +1,178 @@
+'use strict';
+
+module.exports = (function() {
+    return [
+{
+  "name": "rc-collapse",
+  "description": "rc-collapse ui component for react",
+  "repos": "git+ssh://git@github.com/react-component/rc-collapse.git",
+  "main": "./lib/index",
+  "tag": "master",
+  "dependencies": [
+    "classnames@2.x",
+    "rc-animate@2.x",
+    "velocity-animate@1.x"
+  ],
+  "version": "1.3.2",
+  "build": "rm package.json && npm install --prefix . rc-collapse@1.3.2",
+  "mapping": [
+    {
+      "reg": /\bmin\b|__tests__|gulpfile\.js|webpack\.config\.js|gruntfile\.js|test\.js/i,
+      "release": false
+    },
+    {
+      "reg": /^\/node_modules\/rc-collapse\/lib\/(.*)$/i,
+      "release": "lib/$1"
+    },
+    {
+      "reg": /^\/README\.md$/i,
+      "release": "$&"
+    },
+    {
+      "reg": "*",
+      "release": false
+    }
+  ]
+},
+
+{
+  "name": "rc-collapse",
+  "description": "rc-collapse ui component for react",
+  "repos": "git+ssh://git@github.com/react-component/rc-collapse.git",
+  "main": "./lib/index",
+  "tag": "master",
+  "dependencies": [
+    "classnames@2.x",
+    "rc-animate@2.x",
+    "velocity-animate@1.x"
+  ],
+  "version": "1.4.0",
+  "build": "rm package.json && npm install --prefix . rc-collapse@1.4.0",
+  "mapping": [
+    {
+      "reg": /\bmin\b|__tests__|gulpfile\.js|webpack\.config\.js|gruntfile\.js|test\.js/i,
+      "release": false
+    },
+    {
+      "reg": /^\/node_modules\/rc-collapse\/lib\/(.*)$/i,
+      "release": "lib/$1"
+    },
+    {
+      "reg": /^\/README\.md$/i,
+      "release": "$&"
+    },
+    {
+      "reg": "*",
+      "release": false
+    }
+  ]
+},
+
+{
+  "name": "rc-collapse",
+  "description": "rc-collapse ui component for react",
+  "repos": "git+ssh://git@github.com/react-component/rc-collapse.git",
+  "main": "./lib/index",
+  "tag": "master",
+  "dependencies": [
+    "classnames@2.x",
+    "rc-animate@2.x",
+    "velocity-animate@1.x"
+  ],
+  "version": "1.4.1",
+  "build": "rm package.json && npm install --prefix . rc-collapse@1.4.1",
+  "mapping": [
+    {
+      "reg": /\bmin\b|__tests__|gulpfile\.js|webpack\.config\.js|gruntfile\.js|test\.js/i,
+      "release": false
+    },
+    {
+      "reg": /^\/node_modules\/rc-collapse\/lib\/(.*)$/i,
+      "release": "lib/$1"
+    },
+    {
+      "reg": /^\/README\.md$/i,
+      "release": "$&"
+    },
+    {
+      "reg": "*",
+      "release": false
+    }
+  ]
+},
+
+{
+  "name": "rc-collapse",
+  "description": "rc-collapse ui component for react",
+  "repos": "git+ssh://git@github.com/react-component/rc-collapse.git",
+  "main": "./lib/index",
+  "tag": "master",
+  "dependencies": [
+    "classnames@2.x",
+    "rc-animate@2.x",
+    "velocity-animate@1.x"
+  ],
+  "version": "1.4.2",
+  "build": "rm package.json && npm install --prefix . rc-collapse@1.4.2",
+  "mapping": [
+    {
+      "reg": /\bmin\b|__tests__|gulpfile\.js|webpack\.config\.js|gruntfile\.js|test\.js/i,
+      "release": false
+    },
+    {
+      "reg": /^\/node_modules\/rc-collapse\/lib\/(.*)$/i,
+      "release": "lib/$1"
+    },
+    {
+      "reg": /^\/node_modules\/rc-collapse\/assets\/(.*)$/i,
+      "release": "assets/$1"
+    },
+    {
+      "reg": /^\/README\.md$/i,
+      "release": "$&"
+    },
+    {
+      "reg": "*",
+      "release": false
+    }
+  ]
+},
+
+{
+  "name": "rc-collapse",
+  "description": "rc-collapse ui component for react",
+  "repos": "git+ssh://git@github.com/react-component/rc-collapse.git",
+  "main": "./lib/index",
+  "tag": "master",
+  "dependencies": [
+    "classnames@2.x",
+    "rc-animate@2.x",
+    "velocity-animate@1.x"
+  ],
+  "version": "1.4.3",
+  "build": "rm package.json && npm install --prefix . rc-collapse@1.4.3",
+  "mapping": [
+    {
+      "reg": /\bmin\b|__tests__|gulpfile\.js|webpack\.config\.js|gruntfile\.js|test\.js/i,
+      "release": false
+    },
+    {
+      "reg": /^\/node_modules\/rc-collapse\/lib\/(.*)$/i,
+      "release": "lib/$1"
+    },
+    {
+      "reg": /^\/node_modules\/rc-collapse\/assets\/(.*)$/i,
+      "release": "assets/$1"
+    },
+    {
+      "reg": /^\/README\.md$/i,
+      "release": "$&"
+    },
+    {
+      "reg": "*",
+      "release": false
+    }
+  ]
+}
+    ]
+})();
