3cba3a247b76b9e5a2a67bd3dd750000ffecb571
2betop <liaoxuezhi@icloud.com>
2015-12-17 02:25:26 +0800

    optimize tools

--- tools/npm.js
+++ tools/npm.js
@@ -15,8 +15,15 @@ function escapeReg(str) {
   );
 }
 
+rm('-rf', path.join(__dirname, 'node_modules'));
+rm('-rf', path.join(__dirname, 'builds'));
+
+mkdir('-p', path.join(__dirname, 'builds'));
+
 while (args.length) {
   var arg = args.shift();
+
+  if (~packages.indexOf(arg))continue;
 
   var info = exec('npm info ' + arg, {silent:true}).output;
   /({[\s\S]*})/.test(info) && (info = RegExp.$1)
@@ -39,39 +46,65 @@ while (args.length) {
     config.dependencies = dependencies;
   }
 
-  const versions = json.versions.reverse().slice(0, 5);
-
+  var versions = Array.isArray(json.versions) ? json.versions.reverse().slice(0, 5) : [json.version];
+  
   var items = [];
   versions.forEach(function(version) {
     var item = Object.assign({}, config);
     item.version = version;
     item.build = 'rm package.json && npm install --prefix . ' + item.name + '@' + version;
 
-    item.mapping = [
-      {
-        reg: "/\\bmin\\b|__tests__/i",
-        release: false
-      },
+    if (exec('npm install --prefix '+ __dirname +' ' + item.name + '@' + version).code !== 0) {
+      return;
+    }
 
-      {
+    item.mapping = [];
+
+    item.mapping.push({
+      reg: "/\\bmin\\b|__tests__|gulpfile\\.js|webpack\\.config\\.js|gruntfile\\.js|test\\.js/i",
+      release: false
+    });
+
+    if (test('-d', path.join(__dirname, 'node_modules', item.name, 'lib'))) {
+      item.mapping.push({
         reg: "/^\\/node_modules\\/" + escapeReg(item.name) + "\\/lib\\/(.*)$/i",
         release: 'lib/$1'
-      },
+      });
+    }
 
-      {
+    var main = json.main.replace(/^\.\//, '');
+
+    if (path.dirname(main) === '.' || path.dirname(main) === '') {
+      item.mapping.push({
+        reg: "/^\\/node_modules\\/" + escapeReg(item.name) + "\\/(.*\\.js)$/i",
+        release: '$1'
+      });
+    }
+
+    if (path.dirname(main) === 'dist') {
+      item.mapping.push({
+        reg: "/^\\/node_modules\\/" + escapeReg(item.name) + "\\/dist\\/(.*)$/i",
+        release: '$1'
+      });
+      config.main = item.main = main.replace(/^dist\//, '');
+    }
+
+    if (test('-d', path.join(__dirname, 'node_modules', item.name, 'assets'))) {
+      item.mapping.push({
         reg: "/^\\/node_modules\\/" + escapeReg(item.name) + "\\/assets\\/(.*)$/i",
         release: 'assets/$1'
-      },
+      });
+    }
 
-      {
-        reg: "/^\\/README\\.md$/i",
-        release: '$&'
-      },
-      {
-        reg: '*',
-        release: false
-      }
-    ];
+    item.mapping.push({
+      reg: "/^\\/README\\.md$/i",
+      release: '$&'
+    });
+
+    item.mapping.push({
+      reg: '*',
+      release: false
+    });
 
     var str = JSON.stringify(item, null, 2);
     str = str.replace(/('|")\/(.*)\/([ig]*)\1/g, function(_, quote, value, flag) {
@@ -79,8 +112,13 @@ while (args.length) {
     });
     items.push(str);
   });
+
+  if (!items.length) {
+    continue;
+  }
   
   var contents = PREFIX + items.join(',\n\n') + AFFIX;
   write(path.join(__dirname, "builds", config.name + '.js'), contents);
   console.log('Created %s', config.name + '.js');
+  packages.push(config.name)
 }
