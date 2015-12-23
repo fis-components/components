614025e489bd1542a2b5a4185695f6c84ad13daf
2betop <liaoxuezhi@icloud.com>
2015-12-24 01:18:09 +0800

    update

--- modules/object-assign.js
+++ modules/object-assign.js
@@ -2,32 +2,88 @@
 
 module.exports = (function() {
     return [
-        {
-            repos: 'https://github.com/sindresorhus/object-assign.git',
-            version: '4.0.1',
-            tag: 'master',
-            name: 'object-assign',
-            main: 'index.js',
-            build: 'rm package.json && npm install --prefix . object-assign@4.0.1',
-            
-            mapping: [
-                {
-                    reg: /\bmin\b|__tests__/i,
-                    release: false
-                },
-                {
-                    reg: /^\/node_modules\/object\-assign\/(.*\.js)$/,
-                    release: '$1'
-                },
-                {
-                    reg: /^\/README\.md$/i,
-                    release: '$&'
-                },
-                {
-                    reg: '*',
-                    release: false
-                }
-            ]
-        }
+{
+  "name": "object-assign",
+  "description": "ES6 Object.assign() ponyfill",
+  "repos": "https://github.com/sindresorhus/object-assign.git",
+  "tag": "master",
+  "reposType": "npm",
+  "version": "3.0.0",
+  "build": "rm package.json && npm install --prefix . object-assign@3.0.0",
+  "mapping": [
+    {
+      "reg": /\bmin\b|__tests__|gulpfile\.js|webpack\.config\.js|gruntfile\.js|test\.js/i,
+      "release": false
+    },
+    {
+      "reg": /^\/node_modules\/object-assign\/(.*\.js)$/i,
+      "release": "$1"
+    },
+    {
+      "reg": /^\/README\.md$/i,
+      "release": "$&"
+    },
+    {
+      "reg": "*",
+      "release": false
+    }
+  ]
+},
+
+{
+  "name": "object-assign",
+  "description": "ES6 Object.assign() ponyfill",
+  "repos": "https://github.com/sindresorhus/object-assign.git",
+  "tag": "master",
+  "reposType": "npm",
+  "version": "4.0.0",
+  "build": "rm package.json && npm install --prefix . object-assign@4.0.0",
+  "mapping": [
+    {
+      "reg": /\bmin\b|__tests__|gulpfile\.js|webpack\.config\.js|gruntfile\.js|test\.js/i,
+      "release": false
+    },
+    {
+      "reg": /^\/node_modules\/object-assign\/(.*\.js)$/i,
+      "release": "$1"
+    },
+    {
+      "reg": /^\/README\.md$/i,
+      "release": "$&"
+    },
+    {
+      "reg": "*",
+      "release": false
+    }
+  ]
+},
+
+{
+  "name": "object-assign",
+  "description": "ES6 Object.assign() ponyfill",
+  "repos": "https://github.com/sindresorhus/object-assign.git",
+  "tag": "master",
+  "reposType": "npm",
+  "version": "4.0.1",
+  "build": "rm package.json && npm install --prefix . object-assign@4.0.1",
+  "mapping": [
+    {
+      "reg": /\bmin\b|__tests__|gulpfile\.js|webpack\.config\.js|gruntfile\.js|test\.js/i,
+      "release": false
+    },
+    {
+      "reg": /^\/node_modules\/object-assign\/(.*\.js)$/i,
+      "release": "$1"
+    },
+    {
+      "reg": /^\/README\.md$/i,
+      "release": "$&"
+    },
+    {
+      "reg": "*",
+      "release": false
+    }
+  ]
+}
     ]
 })();
