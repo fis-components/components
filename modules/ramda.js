5cef5917c836a990e9f11e58236c689c58396f98
404pnf <404pnf@exmaple.org>
2015-12-24 16:30:39 +0800

    增加lodash-fp; 使用ramdjs的最新稳定版而不是master

--- modules/ramda.js
+++ modules/ramda.js
@@ -1,11 +1,11 @@
 module.exports = (function () {
     'use strict';
     return [{
-        repos: 'https://github.com/ramda/ramda.git',
-        version: 'master',
+        repos: 'https://github.com/ramda/ramda',
+        version: 'v0.18.0',
         main: 'ramda.js',
         mapping: [{
-            reg: /\/dist\/ramda\.js$/,
+            reg: /\/dist\/ramda\.min\.js$/,
             release: 'ramda.js'
         }, {
             reg: /README\.md$/,
