ec53cc604c47757193ce72922b7bfc95f292d875
2betop <liaoxuezhi@icloud.com>
2015-12-26 21:25:03 +0800

    update

--- modules/react-router.js
+++ modules/react-router.js
@@ -2,39 +2,115 @@
 
 module.exports = (function() {
     return [
-        {
-            repos: 'https://github.com/rackt/react-router.git',
-            version: 'v1.0.0',
-            name: 'react-router',
-            main: 'lib/index.js',
-            build: 'rm package.json && npm install --prefix . react-router@1.0.0',
-            dependencies: [
-                "react@^0.14.0",
-                "invariant@^2.0.0",
-                "warning@^2.0.0",
-                "history@^1.13.1"
-            ],
-            
-            mapping: [
-                 {
-                    reg: /\bmin\b/i,
-                    release: false
-                },
+{
+  "name": "react-router",
+  "description": "A complete routing library for React.js",
+  "repos": "https://github.com/rackt/react-router.git",
+  "main": "lib/index",
+  "tag": "master",
+  "reposType": "npm",
+  "dependencies": [
+    "invariant@^2.0.0",
+    "warning@^2.0.0"
+  ],
+  "version": "1.0.0",
+  "build": "rm package.json && npm install --prefix . react-router@1.0.0",
+  "mapping": [
+    {
+      "reg": /\bmin\b|__tests__|gulpfile\.js|webpack\.config\.js|gruntfile\.js|test\.js/i,
+      "release": false
+    },
+    {
+      "reg": /^\/node_modules\/react-router\/lib\/(.*)$/i,
+      "release": "lib/$1"
+    },
+    {
+      "reg": /^\/README\.md$/i,
+      "release": "$&"
+    },
+    {
+      "reg": /^\/LICENSE\.md$/i,
+      "release": "$&"
+    },
+    {
+      "reg": "*",
+      "release": false
+    }
+  ]
+},
 
-                {
-                    reg: /^\/node_modules\/react\-router\/lib\/(.*)$/,
-                    release: 'lib/$1'
-                },
+{
+  "name": "react-router",
+  "description": "A complete routing library for React.js",
+  "repos": "https://github.com/rackt/react-router.git",
+  "main": "lib/index",
+  "tag": "master",
+  "reposType": "npm",
+  "dependencies": [
+    "invariant@^2.0.0",
+    "warning@^2.0.0"
+  ],
+  "version": "1.0.1",
+  "build": "rm package.json && npm install --prefix . react-router@1.0.1",
+  "mapping": [
+    {
+      "reg": /\bmin\b|__tests__|gulpfile\.js|webpack\.config\.js|gruntfile\.js|test\.js/i,
+      "release": false
+    },
+    {
+      "reg": /^\/node_modules\/react-router\/lib\/(.*)$/i,
+      "release": "lib/$1"
+    },
+    {
+      "reg": /^\/README\.md$/i,
+      "release": "$&"
+    },
+    {
+      "reg": /^\/LICENSE\.md$/i,
+      "release": "$&"
+    },
+    {
+      "reg": "*",
+      "release": false
+    }
+  ]
+},
 
-                {
-                    reg: /^\/README\.md$/i,
-                    release: '$&'
-                },
-                {
-                    reg: '*',
-                    release: false
-                }
-            ]
-        }
+{
+  "name": "react-router",
+  "description": "A complete routing library for React.js",
+  "repos": "https://github.com/rackt/react-router.git",
+  "main": "lib/index",
+  "tag": "master",
+  "reposType": "npm",
+  "dependencies": [
+    "invariant@^2.0.0",
+    "warning@^2.0.0"
+  ],
+  "version": "1.0.3",
+  "build": "rm package.json && npm install --prefix . react-router@1.0.3",
+  "mapping": [
+    {
+      "reg": /\bmin\b|__tests__|gulpfile\.js|webpack\.config\.js|gruntfile\.js|test\.js/i,
+      "release": false
+    },
+    {
+      "reg": /^\/node_modules\/react-router\/lib\/(.*)$/i,
+      "release": "lib/$1"
+    },
+    {
+      "reg": /^\/README\.md$/i,
+      "release": "$&"
+    },
+    {
+      "reg": /^\/LICENSE\.md$/i,
+      "release": "$&"
+    },
+    {
+      "reg": "*",
+      "release": false
+    }
+  ]
+}
     ]
 })();
