16b6931d92f5e073d0bee9b52bf0b6e939cd9d3a
2betop <liaoxuezhi@icloud.com>
2016-01-11 22:43:15 +0800

    update

--- tools/checktask.js
+++ tools/checktask.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 const path = require('path');
 const read = fs.readFileSync;
 const write = fs.writeFileSync;
+var packages = [];
 
 var taskFile = path.join(__dirname, '../task.yml');
 var taskLogFile = path.join(__dirname, '../task.yml.log');
@@ -14,29 +15,44 @@ if (!test('-f', taskFile)) {
 
 if (test('-f', taskLogFile)) {
   var lastSuccessMessageId = read(taskLogFile, 'utf8').trim();
-
-  var ret = exec('git diff --name-status ' + lastSuccessMessageId + '..HEAD');
+  var range = lastSuccessMessageId + '..HEAD';
+  var ret = exec('git diff --name-status ' + range);
 
   if (ret.code) {
-    ret = exec('git diff --name-status HEAD^..HEAD');
+    range = 'HEAD^..HEAD';
+    ret = exec('git diff --name-status ' + range);
   }
   var result = ret.output;
   if (!/\stask\.yml\s/.test(result)) {
-    console.log("Nothing changed, skip!")
+    console.log("task.yml did not change, skip!")
     exit();
-  } 
+  }
+
+  result = exec('git diff ' + range + ' -- task.yml').output;
+  result.replace(/^\+\s+\-\s(.*)$/img, function(_, name) {
+    packages.push(name);
+  });
+
+  if (!packages.length) {
+    console.log('Detected nothing!');
+    exit();
+  } else {
+    console.log('The following packages will be get upgraded. \n\x1b[32m%s\x1b[0m', packages.join('\n'));
+  }
 }
 
-var task = read(taskFile, 'utf8');
+if (!packages.length) {
+  var task = read(taskFile, 'utf8');
+
+  config = yaml.load(task);
+  packages = config.sync.npm;
+
+  if (!Array.isArray(packages)) {
+    packages = [packages];
+  }
+}
+
 var currentCommitId=exec('git rev-parse HEAD').output.trim();
-
-config = yaml.load(task);
-var packages = config.sync.npm;
-
-if (!Array.isArray(packages)) {
-  packages = [packages];
-}
-
 var comment = 'node ' + path.join(__dirname, './npm.js') + ' ' + packages.map(function(item) {
   return '"' + item + '"';
 }).join(' ') + '';
