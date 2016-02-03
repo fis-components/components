d3ef48112e18ed52d0abc84e53b067a7699c8550
zhangjunah <zhangjun19870402@126.com>
2016-02-03 18:14:17 +0800

    Create formsy-material-ui.js

---
+++ modules/formsy-material-ui.js
@@ -0,0 +1,14 @@
+
+'use strict';
+
+module.exports = (function() {
+    return [
+        {
+            repos: 'https://github.com/mbrookes/formsy-material-ui.git',
+            version: '0.3.6',
+            name: 'formsy-material-ui',
+            main: './lib/index.js',
+            umd2commonjs: false
+        }
+    ]
+})();
