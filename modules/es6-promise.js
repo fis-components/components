65c0fd9ff6c98dc731f1814b8563899d8af88b23
justin <justinz@olymtech.com>
2016-02-22 10:19:53 +0800

    update es-promise.js

--- modules/es6-promise.js
+++ modules/es6-promise.js
@@ -21,5 +21,25 @@ module.exports = (function() {
                 release: false
             }
         ]
+    },{
+        repos: 'https://github.com/components/es6-promise',
+        version: 'v3.1.2',
+        description: 'An es6-promise JavaScript polyfill.',
+        name: 'es6-promise',
+        main: 'es6-promise.js',
+        mapping: [
+            {
+                reg: /^\/es6-promise\.js$/,
+                release: 'es6-promise.js'
+            },
+            {
+                reg: /^\/README\.md$/,
+                release: '$&'
+            },
+            {
+                reg: '*',
+                release: false
+            }
+        ]
     }]
 })();
